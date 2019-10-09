7fc1475e4dbdbee5f1708c3c628986197ee997fb
yilinma28 <ma.yil@husky.neu.edu>
2019-10-09 18:56:18 +0000

    included zip to address name method

---
+++ zip_to_address_name.py
@@ -0,0 +1,30 @@
+import json
+from botocore.vendored import requests
+
+def zip_to_address_name(zip_code):
+    zip_code = zip_code.strip()
+    
+    if zip_code == '':
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Resquest must contain the ZIP code.')
+        }
+        
+    if not zip_code.isnumeric() or len(zip_code) != 5:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Bad format! The ZIP code should be a 5 digit number.')
+        }
+        
+    
+    geocoding_url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    payload = {'address': zip_code, 'key': 'AIzaSyBfai_EdC2MTJ3OJl2AUBzhScAuV7CAp9Y'}
+    
+    response = requests.get(geocoding_url, params=payload)
+    address = response.json()['results'][0]['formatted_address']
+    
+    return address
+    
+    
+if __name__ == "__main__":
+    print(zip_to_address_name('10001'))
